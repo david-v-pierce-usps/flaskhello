c57f541b7b774730bc65ed200f27b4dc7e397922
Dave Pierce <David.V.Pierce@usps.gov>
2025-04-04 14:19:05 -0500

    M  app.py

--- app.py
+++ app.py
@@ -2,11 +2,18 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/<path:subpath>')
-def hello():
-  page = f"You requested /{subpath}\n\nThank you for querying the flaskhello app."
+def response(value = None):
+  if not value:
+    value = ""
+  return f"You requested /{value}<br><br>Thank you for querying the flaskhello app."
 
-  return page
+@app.route('/')
+def nothing():
+  return response()
+
+@app.route('/<subpath>')
+def hello(subpath):
+  return response(subpath)
 
 if __name__ == '__main__':
   app.run(debug=True)
